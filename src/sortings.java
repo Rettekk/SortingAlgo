1394239ca613e94ed3243bc9264076d62bbc429b
Benny <empty>
2023-02-28 11:25:25 +0100

    Initial commit

---
+++ src/sortings.java
@@ -0,0 +1,31 @@
+public class sortings {
+    public static void main(String[] args) {
+
+    }
+
+
+    public void bubbleSort() {
+
+    }
+
+    public void selectionSort() {
+
+    }
+
+    public void mergeSort() {
+
+    }
+
+    public void quickSort() {
+
+    }
+
+    public void heapSort() {
+
+    }
+
+    public void generateNumbers() {
+
+    }
+
+}
